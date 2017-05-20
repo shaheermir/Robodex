411c522af3a8e7484b47fe6ac2e8d78909e43ca4
Shaheer Mir <shaheermir0@gmail.com>
2017-05-20 17:25:14 -0400

    Removed Routing Before Starting SSR

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
 const Card = ({ id, name, email }) => {
   return (
-    <Link to={`/profile/${id}`}>
-      <div className='bg-light-green dib pa3 ma2 tc br3 grow'>
-        <img
-          src={`//robohash.org/${id}?200x200`}
-          alt='RoboImg'
-          role='presentation'
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    </Link>
+    <div className='bg-light-green dib pa3 ma2 tc br3 grow'>
+      <img
+        src={`//robohash.org/${id}?200x200`}
+        alt='RoboImg'
+        role='presentation'
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,21 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './Containers/App/App'
-import Profile from './Containers/Profile/Profile'
 import createStore from './store'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 const store = createStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact component={App} />
-        <Route path='/profile/:id' component={Profile} />
-        <Redirect to='/' />
-      </Switch>
-    </BrowserRouter>
+    <App />
   </Provider>,
   document.getElementById('root')
 )
